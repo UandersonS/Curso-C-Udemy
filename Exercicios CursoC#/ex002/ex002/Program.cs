22b8702b5fb087ad193d0374ca6d58031cb879ed
UandersonS <58696209+UandersonS@users.noreply.github.com>
2020-01-04 18:52:30 -0300

    Add files via upload

---
+++ Exercicios CursoC#/ex002/ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int soma = A + B;
+            Console.WriteLine($"SOMA = {soma}");
+        }
+    }
+}
